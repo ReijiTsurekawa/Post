af22245947a5c64e7acdf60d90bafe09b97d8e42
Tsurekawa <reiji.judo@gmail.com>
2019-02-15 13:36:06 +0900

    ha?

--- P+P/Assets/Scripts/bikeMove.cs
+++ P+P/Assets/Scripts/bikeMove.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bikeMove : MonoBehaviour {
-    float speed=0;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate() {
+public class bikeMove : MonoBehaviour
+{
+    float speed = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         if (speed < 0)
             speed = 0;
         if (Input.GetKey(KeyCode.UpArrow))
@@ -20,13 +23,13 @@ public class bikeMove : MonoBehaviour {
         else
         {
             if (speed != 0)
-                speed = speed-0.1f;
+                speed = speed - 0.1f;
         }
-        this.transform.Translate(1f + speed, 0,0);
+        this.transform.Translate(1f + speed, 0, 0);
         if (Input.GetKey(KeyCode.RightArrow))
-            this.transform.Rotate(new Vector3(0,-1f,0));
+            this.transform.Rotate(new Vector3(0, -1f, 0));
         if (Input.GetKey(KeyCode.LeftArrow))
-            this.transform.Rotate(new Vector3(0,1f,0));
-        
+            this.transform.Rotate(new Vector3(0, 1f, 0));
+
     }
 }
